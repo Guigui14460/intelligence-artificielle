3ecf70dbbcafa90e0efb54b07fc48836d9d24078
Guigui14460 <letgui2@gmail.com>
2020-11-13 09:57:51 +0100

    Ajout d'un titre pour le package test

---
+++ src/test/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package permettant de tester les différents package de l'application.
+ * 
+ * @since 1.0
+ * @version 1.0
+ * @author Arthur BOCAGE, Guillaume LETELLIER, Corentin PIERRE, Alexandre
+ *         PIGNARD
+ */
+package test;
